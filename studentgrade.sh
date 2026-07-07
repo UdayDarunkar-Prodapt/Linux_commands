758b2219847d26d128862d90b954cce4742a79e0
UdayDarunkar-Prodapt <uday.shridhar@prodapt.com>
2026-07-07 16:38:39 +0530

    Add files via upload

---
+++ studentgrade.sh
@@ -0,0 +1,27 @@
+echo "Enter the 1st subject mark: "
+read a
+echo "Enter the 2nd subject mark:"
+read b 
+echo "Enter the 3rd subject mark:"
+read c
+echo "Enter the 4th subject mark:"
+read d
+echo "Enter the 5th subject mark: "
+read e
+
+total = $((a + b + c + d + e))
+average = $((total/5))
+
+# display 
+echo "Total marrks $total"
+echo "Average marks $average"
+
+# display the grade 
+if [ average -ge 90 ]; then 
+  grade = "A+"
+elif [ average -ge 80 ]; then 
+  grade = "B+"
+else grade = "C"
+fi
+
+echo "Grade $grade"
