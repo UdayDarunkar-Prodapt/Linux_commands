7879e892f92d12b0e3a1b1e5fc52e2a52d947cd9
UdayDarunkar-Prodapt <uday.shridhar@prodapt.com>
2026-07-08 15:09:33 +0530

    Add files via upload

---
+++ payyroll.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Employee Payroll"
+echo "----------------------------"
+
+while read id name salary
+do
+    
+    [ -z "$salary" ] && continue
+
+    
+    [[ ! "$salary" =~ ^[0-9]+$ ]] && continue
+
+    if [ "$salary" -le 30000 ]
+    then
+        tax=$((salary * 5 / 100))
+
+    elif [ "$salary" -le 60000 ]
+    then
+        tax=$((salary * 10 / 100))
+
+    else
+        tax=$((salary * 15 / 100))
+    fi
+
+    if [ "$salary" -lt 50000 ]
+    then
+        bonus=2000
+    else
+        bonus=5000
+    fi
+
+    net=$((salary - tax + bonus))
+
+    echo "$id $name $salary $tax $bonus $net"
+
+done < emp.txt
